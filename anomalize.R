29f02ae7ba7a02686a86b2170ec844a05328c87c
amrrs <amrrs@outlook.com>
2018-04-15 13:07:14 +0530

    Create anomalize.R

---
+++ anomalize.R
@@ -0,0 +1,21 @@
+library(anomalize)
+library(tidyverse)
+library(coindeskr)
+
+
+btc <- get_historic_price(start = "2017-01-01")
+
+btc_ts <- btc %>% rownames_to_column() %>% as.tibble() %>% 
+  mutate(date = as.Date(rowname)) %>% select(-one_of('rowname'))
+
+btc_ts %>% 
+  time_decompose(Price) %>%
+    anomalize(remainder) %>%
+    time_recompose() %>%
+    plot_anomalies(time_recomposed = TRUE, ncol = 3, alpha_dots = 0.5)
+
+
+btc_ts %>% 
+time_decompose(Price, method = "stl", frequency = "auto", trend = "auto") %>%
+    anomalize(remainder, method = "gesd", alpha = 0.05, max_anoms = 0.2) %>%
+    plot_anomaly_decomposition() 
